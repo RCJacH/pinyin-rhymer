518f580494862172782d569791dd1c4b278d1eac
RCJacH <RCJacH@outlook.com>
2022-07-10 17:37:00 +0800

    add consonant.py with tests

---
+++ tests/test_consonant.py
@@ -0,0 +1,26 @@
+import pytest
+
+from pinyin_rhymer.consonant import Consonant
+
+CONSONANT_FAMILIES = {
+    'Plosive': 'p t k b d g'.split(),
+    'Fricative': 'f x h s sh'.split(),
+    'Affricate': 'j q z zh c ch'.split(),
+    'Lateral': 'l r'.split(),
+    'Nasal': 'm n'.split()
+}
+
+
+@pytest.fixture(
+    params=((family, x) for family in CONSONANT_FAMILIES
+            for x in CONSONANT_FAMILIES[family]),
+    ids=lambda x: '_'.join(x)
+)
+def consonant_cases(request):
+    family, x = request.param
+    return pytest.param(getattr(Consonant, family), x)
+
+
+def test_hasattr(consonant_cases):
+    family, x = consonant_cases.values
+    assert hasattr(family, x)
